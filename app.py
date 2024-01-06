9c2c171613159a55958ccfaf3cfa7dcd9ff81e3c
Rupachinthakuntla <155796079+Rupachinthakuntla@users.noreply.github.com>
2024-01-06 12:25:05 +0530

    created ml model

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ Y=dataset.iloc[:,-1].values
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 
 classifier=LogisticRegression()
-classifier,fit(X_train,Y_train)
+classifier.fit(X_train,Y_train)
 
 st.title('Crop Recommendation')
 n=st.number_input('Enter Nitrogen:')
-p=st.number_input('Enter Phosphorus:')
+p=st.number_input('Enter Phosphorous:')
 k=st.number_input('Enter Potassium:')
 t=st.number_input('Enter Temperature:')
 h=st.number_input('Enter Humidity:')
 ph=st.number_input('Enter pH:')
 r=st.number_input('Enter Rainfall:')
 
-if st.button('Recomend Crop'):
+if st.button('Recommend Crop'):
     data=[[n,p,k,t,h,ph,r]]
     result=classifier.predict(data)[0]
     st.success(result)
