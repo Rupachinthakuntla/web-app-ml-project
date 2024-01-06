3c74cfca29085b33e2280f2f784d3b28073df8ab
Rupachinthakuntla <155796079+Rupachinthakuntla@users.noreply.github.com>
2024-01-06 11:47:59 +0530

    created ml model

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from sklearn.linear_model import LogisticRegression
 import streamlit as st
 
 dataset=pd.read_csv('crop.csv')
-x=dataset.iloc[:,:-1].values
-y=dataset.iloc[:,-1].values
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 
 classifier=LogisticRegression()
-classifier,fit(x_train,y_train)
+classifier,fit(X_train,Y_train)
 
-st.title('crop recommendation')
+st.title('Crop Recommendation')
 n=st.number_input('Enter Nitrogen:')
 p=st.number_input('Enter Phosphorus:')
 k=st.number_input('Enter Potassium:')
